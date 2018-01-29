6d3d63954c405ac1513d28bb5ab0969f559a672a
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-01-29 13:44:09 -1000

    Day 7 javascript practice

--- app.js
+++ app.js
@@ -405,3 +405,29 @@ function sieveOfEratoshenes(num) {
 sieveOfEratoshenes(20);
 
 ////////////////////////////////////////////////////////////////////////////////
+//                              Bubble Sort
+
+function bubbleSort(array) {
+  for (var i = array.length; i > 0; i--) {
+    for(var j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        var temp = array[j];
+        array[j] = array[j + 1];
+        array[j+1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+
+bubbleSort([5,3,8,2,1,4]);
+
+bubbleSort([50,20,-20,31,1,-52,-12,12]);
+
+
+////////////////////////////////////////////////////////////////////////////////
+//                              Merge Sort
+
+
+////////////////////////////////////////////////////////////////////////////////
